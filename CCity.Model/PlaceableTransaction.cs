cc1ec845c0ad08039af0e1924231aa89d5bf4a3c
Martin Solti <beszn1@inf.elte.hu>
2023-07-02 16:05:51 +0200

    Added documentation comments to ITransaction, Placeable, PlaceableTransaction, Spreader, TaxTransaction, Utilities, Zone

--- CCity.Model/PlaceableTransaction.cs
+++ CCity.Model/PlaceableTransaction.cs
@@ -3,9 +3,25 @@
 public class PlaceableTransaction : ITransaction
 {
     #region Properties
+
+    /// <summary>
+    /// The placeable to which the transaction refers
+    /// </summary>
     public Placeable Placeable { get; set; } = null!;
+
+    /// <summary>
+    /// Addition or subtraction
+    /// </summary>
     public bool Add { get; set; }
+
+    /// <summary>
+    /// The amount of the transaction
+    /// </summary>
     public uint Amount { get; set; }
+
+    /// <summary>
+    /// The type of the transaction
+    /// </summary>
     public PlaceableTransactionType TransactionType { get; set; }
     #endregion
 }
